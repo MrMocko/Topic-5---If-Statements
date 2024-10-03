3905d57fee7f0e1993d868226631ea07635c34d5
Andre Mocko <339723967@GoTVDSB.ca>
2024-10-03 09:27:54 -0400

    Add project files.

---
+++ Topic 5  - If Statements/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_5____If_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Andre
+            int talbots = 20;
+            int maxthompsonJrs = 30;
+            int demos = 15;
+            Console.WriteLine("Talbots: " + talbots + " Max Thompson Jr: " + maxthompsonJrs + " Demos: " + demos);
+            if (talbots < maxthompsonJrs)
+            {
+                Console.WriteLine("SO MANY MAX THOMPSON JR'S! THE WORLD IS SAVED");
+            }
+        }
+    }
+}

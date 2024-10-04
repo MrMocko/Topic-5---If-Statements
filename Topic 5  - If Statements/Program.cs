eeada3b1d99939a7966eaf1675ebe260c5e58066
Andre Mocko <339723967@GoTVDSB.ca>
2024-10-04 09:37:44 -0400

    not fone

--- Topic 5  - If Statements/Program.cs
+++ Topic 5  - If Statements/Program.cs
@@ -4,14 +4,45 @@
     {
         static void Main(string[] args)
         { //Andre
-            int talbots = 20;
-            int maxthompsonJrs = 30;
-            int demos = 15;
-            Console.WriteLine("Talbots: " + talbots + " Max Thompson Jr: " + maxthompsonJrs + " Demos: " + demos);
-            if (talbots < maxthompsonJrs)
+            int people = 20;
+            int cats = 20;
+            int dogs = 15;
+            Console.WriteLine("People: " + people + " Cats: " + cats + " Dogs: " + dogs);
+            if (people < cats)
             {
-                Console.WriteLine("SO MANY MAX THOMPSON JR'S! THE WORLD IS SAVED");
+                Console.WriteLine("Too many Cats, THE WORLD IS DOOMED!");
             }
-        }
+            if (people > cats)
+            {
+                Console.WriteLine("The world is drooled on!");
+            }
+            if(people > dogs)
+            {
+                Console.WriteLine("The world is dry!");
+            }
+            Console.WriteLine("Press ENTER to continue. ");
+            Console.ReadLine();
+            Console.Clear();
+            dogs += 5;
+            Console.WriteLine("People: " + people + " Dogs: " + dogs + " Cats: " + cats);
+            if (people >= dogs)
+            {
+                Console.WriteLine("People are greater than or equal to dogs.");
+            }
+            if (people <= dogs)
+            {
+                Console.WriteLine("People are less than or equal to dogs.");
+            }
+            if (people == dogs)
+            {
+                Console.WriteLine("People are dogs.");
+            }
+            // If's are peices of code that if put in can run some code if their are certin conditions met.
+            // The curley braces are used to put the code that the "if" would need to run
+            if (condition)
+            {
+                // Code here will only be run if 
+            }
+        }   
     }
 }

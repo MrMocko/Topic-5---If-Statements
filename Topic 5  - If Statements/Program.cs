3a1a294c9e0f07e69dda2a58d52db043d4c548a7
Andre Mocko <339723967@GoTVDSB.ca>
2024-10-07 09:38:08 -0400

    not done

--- Topic 5  - If Statements/Program.cs
+++ Topic 5  - If Statements/Program.cs
@@ -7,6 +7,9 @@
             int people = 20;
             int cats = 20;
             int dogs = 15;
+            string dinosaur;
+            string please, Please, PLEASE;
+            int age;
             Console.WriteLine("People: " + people + " Cats: " + cats + " Dogs: " + dogs);
             if (people < cats)
             {
@@ -39,10 +42,37 @@
             }
             // If's are peices of code that if put in can run some code if their are certin conditions met.
             // The curley braces are used to put the code that the "if" would need to run
-            if (condition)
+            Console.WriteLine("What famous dinosaur has three large horns?");
+            dinosaur = Console.ReadLine();
+            if (dinosaur.ToLower () == "yes")
             {
-                // Code here will only be run if 
+                Console.WriteLine("You are corrct");
             }
+            Console.WriteLine("What is the magic word? ");
+            please = Console.ReadLine();
+            if (please.ToLower () == "please")
+            {
+                Console.WriteLine("You're welcome ");
+            }
+            int.TryParse(Console.ReadLine(), out age);
+            if (age < 16)
+            {
+                Console.WriteLine("You can't drive yet!");
+            }
+            if (age < 18)
+            {
+                Console.WriteLine("You can't vote yet bozo");
+            }
+            if (age < 25)
+            {
+                Console.WriteLine("You can't rent are car yet");
+            }
+            if (age > 25)
+            {
+                Console.WriteLine("You can do anyhing that's legal");
+            }
+
+
         }   
     }
 }
